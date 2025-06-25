5a0eb59e7d209a4f9609ef4a6ab5e7e74b646afc
catc4ke <julianjenkinson@hotmail.com>
2025-06-25 23:11:49 +1000

    Filter bar styled - not linked

--- components/FilterDrawer.tsx
+++ components/FilterDrawer.tsx
@@ -1,11 +1,15 @@
 import { Box, HStack, Pressable, Text } from '@gluestack-ui/themed';
+import Slider from '@react-native-community/slider';
 import React, { useState } from 'react';
-import { Modal, StyleSheet } from 'react-native';
+import { Modal, StyleSheet, View } from 'react-native';
+
 
 type FilterDrawerProps = {
   open: boolean;
   onClose: () => void;
 };
+
+const marks = ["0hr", '1hr', '2hr', '3hr+'];
 
 export default function FilterDrawer({ open, onClose }: FilterDrawerProps) {
   
@@ -28,10 +32,10 @@ export default function FilterDrawer({ open, onClose }: FilterDrawerProps) {
         {/* This inner View prevents the press event from propagating to the overlay */}
         <Pressable
           onPress={(e) => e.stopPropagation()}
-          style={{ backgroundColor: 'white', padding: 16, borderTopLeftRadius: 20, borderTopRightRadius: 20, height: 600 }}
+          style={{ backgroundColor: 'white', padding: 16, borderTopLeftRadius: 20, borderTopRightRadius: 20, height: 660 }}
         >
-          <Text fontFamily="Nunito-800" size="2xl" color='#000' py={8} pt={14}>Sort By</Text>
-          <Box flexDirection="row" flexWrap="wrap" gap={12} pb={12}>
+          <Text fontFamily="Nunito-800" size="2xl" color='#000' pb={14} pt={14}>Sort By</Text>
+          <Box flexDirection="row" flexWrap="wrap" gap={12} pb={8}>
             <Pressable style={styles.pill}>
               <Text style={styles.pillText}>Newest</Text>
             </Pressable>
@@ -39,20 +43,17 @@ export default function FilterDrawer({ open, onClose }: FilterDrawerProps) {
               <Text style={styles.pillText}>Oldest</Text>
             </Pressable>
             <Pressable style={styles.pill}>
-              <Text style={styles.pillText}>A - Z</Text>
+              <Text style={styles.pillText}>Alphabetical</Text>
             </Pressable>
             <Pressable style={styles.pill}>
               <Text style={styles.pillText}>Prep Time</Text>
             </Pressable>
-            <Pressable style={styles.pill}>
-              <Text style={styles.pillText}>A - Z</Text>
-            </Pressable>
           </Box>
 
-          <Text fontFamily="Nunito-800" size="2xl" color='#000' py={8} pt={14}>Filters</Text>
+          <Text fontFamily="Nunito-800" size="2xl" color='#000' pb={14} pt={16}>Filters</Text>
           
-          <Text fontFamily="Nunito-800" size="lg" color='#000'pb={8}>Category</Text>
-          <Box flexDirection="row" flexWrap="wrap" gap={12} pb={12}>
+          <Text fontFamily="Nunito-800" size="lg" color='#000'pb={10}>Category</Text>
+          <Box flexDirection="row" flexWrap="wrap" gap={12} pb={16}>
             <Pressable style={styles.pill}>
               <Text style={styles.pillText}>Breakfast</Text>
             </Pressable>
@@ -60,10 +61,10 @@ export default function FilterDrawer({ open, onClose }: FilterDrawerProps) {
               <Text style={styles.pillText}>Appetiser</Text>
             </Pressable>
             <Pressable style={[styles.pill, pressed && styles.pillPressed]}
-      onPressIn={() => setPressed(true)}
-      onPressOut={() => setPressed(false)}
-      >
-              <Text style={styles.pillText}>Mains</Text>
+              onPressIn={() => setPressed(true)}
+              onPressOut={() => setPressed(false)}
+            >
+              <Text style={styles.pillText}>Main</Text>
             </Pressable>
             <Pressable style={styles.pill}>
               <Text style={styles.pillText}>Vegetarian</Text>
@@ -76,19 +77,59 @@ export default function FilterDrawer({ open, onClose }: FilterDrawerProps) {
             </Pressable>
           </Box>
 
-          <Text fontFamily="Nunito-800" size="lg" color='#000'pb={8}>Difficulty</Text>
-          <Box flexDirection="row" flexWrap="wrap" gap={12} pb={12}>
+          <Text fontFamily="Nunito-800" size="lg" color='#000'pb={10}>Difficulty</Text>
+          <Box flexDirection="row" flexWrap="wrap" gap={12} pb={16}>
             <Pressable style={styles.pill}>
-              <Text style={styles.pillText}> Difficulty Selector</Text>
+              <Text style={styles.pillText}>Easy</Text>
+            </Pressable>
+            <Pressable style={styles.pill}>
+              <Text style={styles.pillText}>Medium</Text>
+            </Pressable>
+            <Pressable style={styles.pill}>
+              <Text style={styles.pillText}>Hard</Text>
             </Pressable>
           </Box>
 
           <Text fontFamily="Nunito-800" size="lg" color='#000'pb={8}>Cook Time</Text>
-          <Box flexDirection="row" flexWrap="wrap" gap={12} pb={12}>
-            <Pressable style={styles.pill}>
-              <Text style={styles.pillText}>Cook Time Selector</Text>
-            </Pressable>
-          </Box>
+          {/*<Text fontSize={16} fontWeight="600" mb={2}>
+            Max Cook Time: {maxCookTime} min
+          </Text>*/}
+          <Slider
+            style={{ width: '100%', height: 30 }}
+            minimumValue={0}
+            maximumValue={240}
+            step={4}
+            minimumTrackTintColor="#0A192F"
+            maximumTrackTintColor="#333"
+            thumbTintColor="#0A192F"
+            //value={maxCookTime}
+            //onValueChange={setMaxCookTime}
+          />
+          {/* Marks */}
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingHorizontal: 10,
+              //marginTop: 0,
+            }}
+          >
+            {marks.map((mark) => (
+              
+              <Text
+                key={mark}
+                style={{
+                  fontSize: 12,
+                  color: '#000',
+                  fontFamily: 'Nunito-500',
+                  textAlign: 'center',
+                  width: 26,
+                }}
+              >
+                {mark}
+              </Text>
+            ))}
+          </View>
 
           <HStack justifyContent="space-between" mt="auto" mx={25} pt={12}>
             <Pressable
